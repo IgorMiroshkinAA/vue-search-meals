25212b5dcee2091140f5e0243eccad4f7f713b9e
Игорь Александрович Мирошкин <barb0s9109@mail.ru>
2024-08-27 13:58:16 +0300

    initial commit

---
+++ src/routes/index.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from "../views/HomeView.vue";
+import MealList from "../views/MealList.vue";
+
+const routes = [
+    {
+        path: '/',
+        name: 'home',
+        component: HomeView
+    },
+    {
+        path: '/letter/:letter',
+        name: 'byLetter',
+        component: MealList
+    }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+export default router
---
+++ src/views/HomeView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="flex flex-col p-8 justify-center">
+    <input type="text" class="rounded border-2 border-gray-200 w-full" placeholder="Search for meals">
+
+    <div class="flex gap-2 justify-center mt-2">
+      <router-link
+          :key="letter"
+          :to="{name: 'byLetter', params: {letter}}"
+          v-for="letter of letters"
+      >
+        {{ letter }}
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import {computed, onMounted} from "vue";
+import store from "../store/index.js";
+import axiosClient from "../axiosClient.js";
+
+const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
+
+onMounted(async () => {
+  const response = await axiosClient.get('/list.php?i=list')
+  console.log(response.data)
+})
+</script>
+
+<style scoped>
+
+</style>
